be686fb3d390ce8169ac99242809598c61101821
jhonashc <jhonhuiracocha123456789@gmail.com>
2024-04-13 20:23:25 -0500

    pages and routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'cart',
+    loadComponent: () =>
+      import('./pages/shopping-cart/shopping-cart.component').then(
+        (m) => m.ShoppingCartComponent
+      ),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '',
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
---
+++ src/app/pages/shopping-cart/shopping-cart.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './shopping-cart.component.html',
+  styleUrl: './shopping-cart.component.css',
+})
+export class ShoppingCartComponent {}
