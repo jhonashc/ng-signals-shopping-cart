067316b3c78f0ec1df46700a30fd9a3455bd7cb5
jhonashc <jhonhuiracocha123456789@gmail.com>
2024-04-13 20:23:12 -0500

    mock data added

---
+++ src/app/data/products.ts
@@ -0,0 +1,18 @@
+export const products = [
+  {
+    id: 1,
+    title: 'Camisa Oxford Regular Fit',
+    price: 19.99,
+    image: '/assets/images/image-1.webp',
+  },
+  {
+    id: 2,
+    title: 'Camisa de lyocell de manga corta Regular Fit',
+    price: 29.99,
+  },
+  {
+    id: 3,
+    title: 'Camisa resort Loose Fit',
+    price: 31.99,
+  },
+];
